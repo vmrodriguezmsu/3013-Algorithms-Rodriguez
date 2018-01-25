ac279482cf918effeb02dd877392b63f8c2e4788
vmrodriguezmsu <foxsoccer11@yahoo.com>
2018-01-25 12:06:11 -0600

    Update main.cpp

--- assignments/main.cpp
+++ assignments/main.cpp
@@ -1,3 +1,23 @@
+/*
+Title:
+	Program 1
+Files:
+	main.cpp
+Semester:
+	CMPS 3013--Spring 2018
+Author: 
+	Vincent Rodriguez
+Email:
+	foxsoccer11@yahoo.com
+Description:
+	The purpose of this program is to use a priority queue
+	to hold a list of integers ranging from 0 - 99. When the program
+	runs, it will create the container for this list and a pointer
+	called Head. Afterwards, the program will generate random numbers
+	(modulo 100) and use the orderedSert method to keep the integers
+	in order according to their values and when they were received
+	into the list.
+*/
 #include <iostream>
 #include <ctime>
 
